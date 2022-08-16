2a0e579a0de1aa1efda6df840310c8da34d289dd
phawit-s <56498877+phawit-s@users.noreply.github.com>
2022-08-16 14:01:18 +0700

    Add : Frontend project

---
+++ frontend/src/App.js
@@ -0,0 +1,10 @@
+import React, { Component }  from 'react';
+import Home from "./Pages/Home";
+
+function App() {
+  return (
+    <Home />
+  );
+}
+
+export default App;
---
+++ frontend/src/Pages/Home.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Box, Heading, Text, Card, Flex, Link, Button, Image } from "rebass";
+import _ from "lodash";
+
+const Home = () => {
+  return (
+    <Box>
+      <Box
+        mt={1}
+        pt="0.5px"
+        sx={{
+          borderRadius: "16px",
+          boxShadow: "1px 0px 5px 1px rgba(0, 0, 0, 0.1)",
+        }}
+        mx={3}
+      ></Box>
+    </Box>
+  );
+};
+
+export default Home;
