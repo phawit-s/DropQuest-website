b79ea8a1280958be50385b2fe160620ff3397076
phawit-s <phawit.ps@gmail.com>
2023-01-31 19:08:57 +0700

    add folder client

---
+++ client/src/components/Header/index.js
@@ -0,0 +1,240 @@
+import React, { useEffect, useState } from "react";
+import { useHistory, Redirect } from "react-router-dom";
+import { Box, Heading, Text, Card, Flex, Link, Button, Image } from "rebass";
+import { useAuth } from "../../contexts/AuthContext";
+import { motion } from "framer-motion";
+import { AiOutlineCaretDown, AiOutlineCaretUp } from "react-icons/ai";
+
+const Header = () => {
+  const history = useHistory();
+  const { currentUser, logout } = useAuth();
+  const [toggleRoom, setToggleRoom] = useState(false);
+  const [toggleQuiz, setToggleQuiz] = useState(false);
+  const [toggleProfile, setToggleProfile] = useState(false);
+  const [color, setColor] = useState("");
+  const [profilecolor, setProfileColor] = useState("");
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleroom = () => {
+    setToggleRoom(!toggleRoom);
+  };
+  const togglequiz = () => {
+    setToggleQuiz(!toggleQuiz);
+  };
+  const toggleprofile = () => {
+    setToggleProfile(!toggleProfile);
+  };
+
+  const base64Image = new Buffer(currentUser.image, "binary").toString(
+    "base64"
+  );
+  const imageUrl = `data:image/jpeg;base64,${base64Image}`;
+  if (!currentUser) {
+    return <Redirect to="/login" />;
+  }
+
+  const subMenuAnimate = {
+    enter: {
+      opacity: 1,
+      rotateX: 0,
+      transition: {
+        staggerChildren: 0.2,
+        staggerDirection: -1,
+        duration: 0.3,
+      },
+      display: "block",
+    },
+    exit: {
+      opacity: 0,
+      rotateX: -15,
+      transition: {
+        duration: 0.3,
+        delay: 0.1,
+        staggerChildren: 0.2,
+        staggerDirection: 1,
+      },
+      transitionEnd: {
+        display: "none",
+      },
+    },
+  };
+  return (
+    <Flex
+      mx={4}
+      pt={4}
+      justifyContent="right"
+      sx={{
+        "@media (max-width: 768px)": {
+          justifyContent: "center",
+        },
+      }}
+    >
+      <Text
+        fontSize="20px"
+        mr={3}
+        sx={{ cursor: "pointer" }}
+        onClick={() =>
+          history.push({
+            pathname: `/`,
+          })
+        }
+      >
+        หน้าหลัก
+      </Text>
+
+      <Box mr={2}>
+        <motion.div className="menu-item" onMouseDown={toggleroom}>
+          <Flex sx={{ cursor: "pointer" }}>
+            <Text fontSize="20px" width="100px" mb={2}>
+              ห้องของฉัน
+            </Text>
+            <Box mt={2}>
+              {toggleRoom ? <AiOutlineCaretUp /> : <AiOutlineCaretDown />}
+            </Box>
+          </Flex>
+
+          <motion.div
+            className="sub-menu"
+            initial="exit"
+            animate={toggleRoom ? "enter" : "exit"}
+            variants={subMenuAnimate}
+          >
+            <Box
+              width="100px"
+              sx={{
+                backgroundColor: "white",
+                p: 2,
+                borderRadius: "10px",
+                position: "absolute",
+              }}
+            >
+              <Text color="black">สร้างห้อง</Text>
+              <Text color="black">ห้องของฉัน</Text>
+              <Text color="black">สรุปผล</Text>
+            </Box>
+          </motion.div>
+        </motion.div>
+      </Box>
+
+      <Box mr={1}>
+        <motion.div className="menu-item" onMouseDown={togglequiz}>
+          <Flex width="150px" sx={{ cursor: "pointer" }}>
+            <Text fontSize="20px" mb={2}>
+              แบบทดสอบ
+            </Text>
+            <Box mt={2} ml={2}>
+              {toggleQuiz ? <AiOutlineCaretUp /> : <AiOutlineCaretDown />}
+            </Box>
+          </Flex>
+          <motion.div
+            className="sub-menu"
+            initial="exit"
+            animate={toggleQuiz ? "enter" : "exit"}
+            variants={subMenuAnimate}
+          >
+            <Box
+              width="150px"
+              sx={{
+                backgroundColor: "white",
+                p: 2,
+                borderRadius: "10px",
+                position: "absolute",
+              }}
+            >
+              <Text
+                onMouseEnter={() => setColor("#364AFF")}
+                onMouseLeave={() => setColor("")}
+                sx={{ cursor: "pointer", color: color }}
+                onClick={() =>
+                  history.push({
+                    pathname: `/createquiz`,
+                  })
+                }
+              >
+                สร้างแบบทดสอบ
+              </Text>
+              <Text color="black">แบบทดสอบของฉัน</Text>
+            </Box>
+          </motion.div>
+        </motion.div>
+      </Box>
+
+      <Box mr={2}>
+        <motion.div className="menu-item" onMouseDown={toggleprofile}>
+          <Flex width="120px" sx={{ cursor: "pointer" }}>
+            <Text
+              fontSize="20px"
+              mb={2}
+              sx={{
+                overflow: "hidden",
+                textOverflow: "ellipsis",
+                whiteSpace: "nowrap",
+              }}
+            >
+              {currentUser.username}
+            </Text>
+            <Box mt={2} ml={2}>
+              {toggleProfile ? <AiOutlineCaretUp /> : <AiOutlineCaretDown />}
+            </Box>
+          </Flex>
+          <motion.div
+            className="sub-menu"
+            initial="exit"
+            animate={toggleProfile ? "enter" : "exit"}
+            variants={subMenuAnimate}
+          >
+            <Box
+              width="120px"
+              sx={{
+                backgroundColor: "white",
+                p: 2,
+                borderRadius: "10px",
+                position: "absolute",
+              }}
+            >
+              <Text
+                onMouseEnter={() => setProfileColor("#364AFF")}
+                onMouseLeave={() => setProfileColor("")}
+                sx={{ cursor: "pointer", color: profilecolor }}
+                onClick={() =>
+                  history.push({
+                    pathname: `/profile`,
+                  })
+                }
+                fontSize="16px"
+              >
+                โปรไฟล์
+              </Text>
+              <Text
+                sx={{
+                  cursor: "pointer",
+                }}
+                fontSize="16px"
+                color="red"
+                onClick={(e) => {
+                  e.preventDefault();
+                  logout();
+                }}
+              >
+                ออกจากระบบ
+              </Text>
+            </Box>
+          </motion.div>
+        </motion.div>
+      </Box>
+      <Image
+        ml={3}
+        src={imageUrl}
+        referrerPolicy="no-referrer"
+        sx={{
+          borderRadius: "50%",
+          width: "90px",
+          height: "90px",
+          objectFit: "cover",
+        }}
+      />
+    </Flex>
+    //</Flex>
+  );
+};
+
+export default Header;
---
+++ server/public/precache-manifest.0fc81baa106a63567f91ca99e7d39810.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7426ab41e2fc2fb6b0b2",
+    "url": "/static/css/main.43955cd0.chunk.css"
+  },
+  {
+    "revision": "7426ab41e2fc2fb6b0b2",
+    "url": "/static/js/main.7426ab41.chunk.js"
+  },
+  {
+    "revision": "310dddc7432f703a145e",
+    "url": "/static/js/1.310dddc7.chunk.js"
+  },
+  {
+    "revision": "ed57d16f30b882bd8209",
+    "url": "/static/js/2.ed57d16f.chunk.js"
+  },
+  {
+    "revision": "f860ee95184e757bcd55",
+    "url": "/static/js/runtime~main.f860ee95.js"
+  },
+  {
+    "revision": "39aaf4eb997d7bac6b3b70f879dd9910",
+    "url": "/index.html"
+  }
+];
